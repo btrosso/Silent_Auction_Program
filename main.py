f6cd4ae0cbd4843a95d5e49010a622cead5e28af
btrosso <51491995+btrosso@users.noreply.github.com>
2022-02-08 23:30:35 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,35 @@
-from replit import clear
 from art import logo
-#HINT: You can call clear() to clear the output in the console.
+import os
 
 #print the ascii art for the program
-#print(logo)
 
 participants = {}
 more_participants = True
 
 while more_participants == True:
+    print(logo)
     name = input("Please enter your name: ").lower()
     bid_price = int(input("Please enter a bid proce: "))
 
     def add_participant(p_name, bid):
         participants[p_name] = bid
 
-    add_participant(p_name = name, bid = bid_price)
+    add_participant(p_name=name, bid=bid_price)
 
     keep_going = input("Are there more participants who would like to bid? Enter (yes or no): ").lower()
 
     if keep_going == 'no':
         more_participants = False
 
-    clear()
+    os.system("clear")
 
 temp = 0
 winner = "name"
-
 for person, bid in participants.items():
     if bid > temp:
         temp = bid
         winner = person
 
-
-print(winner)
-print(participants)
+print(logo)
+print(f"The winner is: {winner}")
 
